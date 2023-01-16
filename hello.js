3e5f41dd649da525e7c3b4c69d3daf9e77b19ad9
ashadnur71196 <ashadnur1996@gmail.com>
2023-01-16 17:43:04 +0600

    5:42 PMMonday, January 16, 2023 (GMT+6)Time in Dhaka

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+var banabaPrice = 10;
+console.log(typeof banabaPrice);
+var userFirstName = 'John';
+console.log(typeof userFirstName);
+var speech = "lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod.";
+console.log(speech);
+console.log(speech.toUpperCase());
